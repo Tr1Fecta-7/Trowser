f0e982fbda12c9a82acb3109b71b56ab01ccd5d1
Tr1Fecta <tr1develop7@gmail.com>
2019-08-24 13:50:03 +0200

    Initial Commit

---
+++ Trowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Trowser
+//
+//  Created by Tr1Fecta on 24/08/2019.
+//  Copyright © 2019 Tr1Fecta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Trowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Trowser
+//
+//  Created by Tr1Fecta on 24/08/2019.
+//  Copyright © 2019 Tr1Fecta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
